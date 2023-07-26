4b47597577da4a196b914af5a56807e4bb696991
Dominic Buchheit <Dominic.Buchheit@softwareag.com>
2023-07-26 16:19:21 +0200

    Removing debug prints and unused time measures

--- chatgpt_robusta_actions/chat_gpt.py
+++ chatgpt_robusta_actions/chat_gpt.py
@@ -1,5 +1,4 @@
 import logging
-import time
 import json
 
 import cachetools
@@ -40,7 +39,6 @@ def query_chatgtp(params: ChatGPTParams):
         if params.search_term in lru_cache:
             answers = lru_cache[params.search_term]
         else:
-            start_time = time.time()
             input = [
                 {"role": "system", "content": "You are a helpful assistant. Provide kubectl commands only, no explanations!"},
                 {"role": "user",
@@ -55,25 +53,17 @@ def query_chatgtp(params: ChatGPTParams):
                 max_tokens=1000,
                 temperature=0
             )
-            print(res)
             if res:
                 logging.info(f"ChatGPT response: {res}")
-                total_tokens = res.usage['total_tokens']
-                time_taken = time.time() - start_time
                 response_content = res.choices[0].message.content
                 # Store only the main response in the cache
                 lru_cache[params.search_term] = [response_content]
                 answers.append(response_content)
 
-            answers.append(f"\n\n ---")
-            answers.append(
-                f"\n\n | Time taken: {time_taken:.2f} seconds | Total tokens used: {total_tokens} |")
-
     except Exception as e:
         answers.append(f"Error calling ChatCompletion.create: {e}")
         raise
     
-    print(answers)
     return answers
 
 
@@ -94,7 +84,6 @@ def chat_gpt_enricher(alert: PrometheusKubernetesAlert, params: ChatGPTTokenPara
     )
 
     answers = query_chatgtp(action_params)
-    print(answers)
 
     alert.add_enrichment(
         [
