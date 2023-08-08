b69642d23d0dadc4833b1d8a9a6c9b162ccad133
Dominic Buchheit <Dominic.Buchheit@softwareag.com>
2023-08-08 11:48:00 +0200

    Enricher now tries to get all pods and adds it to the query

--- chatgpt_robusta_actions/chat_gpt.py
+++ chatgpt_robusta_actions/chat_gpt.py
@@ -1,6 +1,9 @@
 import logging
 import json
 
+import os
+import requests
+
 import cachetools
 import openai
 from openai.openai_object import OpenAIObject
@@ -23,6 +26,78 @@ class ChatGPTParams(ChatGPTTokenParams):
     """
     search_term: str
     model: str = "gpt-3.5-turbo"
+
+def get_kubernetes_token():
+    token_file = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+    with open(token_file, "r") as f:
+        return f.read().strip()
+    
+def get_pod_name():
+    return os.environ.get("POD_NAME")
+
+def get_container_name():
+    return os.environ.get("CONTAINER_NAME", "your-container-name")
+
+# Use this to list pods
+def get_pods():
+    token = get_kubernetes_token()
+    print(f"XXX KUBERNETES_SERVICE_HOST Environment var: {os.environ.get('KUBERNETES_SERVICE_HOST')}")
+    api_server = "https://kubernetes.default.svc"
+    api_url = f"{api_server}/api/v1/namespaces/default/pods"
+
+    print(f"kubernetes token: {get_kubernetes_token()}")
+    print(f"pd name: {get_pod_name()}")
+    print(f"container name: {get_container_name()}")
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(api_url, headers=headers, verify=False)
+
+    if response.status_code == 200:
+        return f"SUCCESSFUL RESPONSE: {response.json()}"
+    else:
+        return f"Error: {response.status_code}, {response.text}"
+
+# Use this to run custom commands
+def run_kubectl_command_in_pod(namespace, command):
+    # api_server = os.environ.get("KUBERNETES_SERVICE_HOST", "https://kubernetes.default.svc")
+    api_server = "https://kubernetes.default.svc"
+    api_url = f"{api_server}/api/v1/namespaces/{namespace}/pods/{get_pod_name()}/exec"
+
+    print(f"kubernetes token: {get_kubernetes_token()}")
+    print(f"pod name: {get_pod_name()}")
+    print(f"container name: {get_container_name()}")
+    print(f"command: '{command}'")
+
+    headers = {
+        "Authorization": f"Bearer {get_kubernetes_token()}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "apiVersion": "v1",
+        "kind": "Exec",
+        "metadata": {
+            "namespace": namespace,
+            "name": get_pod_name(),
+        },
+        "spec": {
+            "container": get_container_name(),
+            "command": command.split(' '),
+            "stdin": False,
+            "tty": False,
+        }
+    }
+
+    response = requests.post(api_url, headers=headers, json=data, stream=True, verify=False)
+
+    if response.status_code == 200:
+        return f"SUCCESSFUL RESPONSE: {response.json()}"
+    else:
+        return f"Error: {response.status_code}, {response.text}"
 
 
 def query_chatgtp(params: ChatGPTParams):
@@ -61,7 +136,7 @@ def query_chatgtp(params: ChatGPTParams):
                 lru_cache[params.search_term] = [response_content]
                 answers.append(response_content)
             else:
-                logging.error(f"Got no Bedrock response!")
+                logging.error(f"Got no ChatGPT response!")
 
     except Exception as e:
         answers.append(f"Error calling ChatCompletion.create: {e}")
@@ -75,9 +150,10 @@ def chat_gpt_enricher(alert: PrometheusKubernetesAlert, params: ChatGPTTokenPara
     """
     Add a button to the alert - clicking it will ask chat gpt to help find a solution.
     """
+    pods = get_pods()
 
     search_term = ", ".join([f"{key}: {value}" for key, value in alert.alert.labels.items()])
-    search_term = f"{search_term}\n{alert.get_title()}\n{alert.get_description()}"
+    search_term = f"{search_term}\n{alert.get_title()}\n{alert.get_description()}\nPods: {pods}"
     # search_term = json.dumps(alert.alert, indent=1)
     print ('XXX')
     print("XXX Labels: ", search_term)
